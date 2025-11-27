bea135b7e0077b7de5847e440deefbabf424abbb
thernstig <30827238+thernstig@users.noreply.github.com>
2025-11-27 17:06:35 +0100

    refactor: rename SSH_ENV to SSH_ENV_FILE and improve variable names
    Renames SSH_ENV to SSH_ENV_FILE to explicitly indicate it stores a file
    path rather than the environment itself.
    
    Also renames internal variables in _ssh_agent_start for clarity:
    - ssh_output -> agent_output_csh
    - filtered_output -> agent_output_fish

--- conf.d/fish_ssh_agent.fish
+++ conf.d/fish_ssh_agent.fish
@@ -1,5 +1,5 @@
-if test -z "$SSH_ENV"
-    set -xg SSH_ENV "$HOME/.ssh/ssh_agent_env.fish"
+if test -z "$SSH_ENV_FILE"
+    set -xg SSH_ENV_FILE "$HOME/.ssh/ssh_agent_env.fish"
 end
 
 if not _ssh_agent_is_started
--- functions/_ssh_agent_is_started.fish
+++ functions/_ssh_agent_is_started.fish
@@ -12,13 +12,13 @@ function _ssh_agent_is_started --description "Check if ssh agent is already star
     end
 
     # If the SSH environment file exists, source it. This will set SSH_AUTH_SOCK and SSH_AGENT_PID.
-    if test -f "$SSH_ENV"
-        source "$SSH_ENV"
+    if test -f "$SSH_ENV_FILE"
+        source "$SSH_ENV_FILE"
 
         # If SSH_AGENT_PID is still not set, something went wrong.
         # Remove the SSH environment file and indicate the agent is not started.
         if test -z "$SSH_AGENT_PID"
-            rm -f "$SSH_ENV"
+            rm -f "$SSH_ENV_FILE"
             return 1
         end
 
@@ -28,7 +28,7 @@ function _ssh_agent_is_started --description "Check if ssh agent is already star
             return 0
         end
 
-        rm -f "$SSH_ENV"
+        rm -f "$SSH_ENV_FILE"
         return 1
     end
 
